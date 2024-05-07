5420a27a2e7c9476b5a030539e9aa741059a729d
Yassin Taoumi <toumiyassin84@gmail.com>
2024-05-07 02:09:05 +0100

    version

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
 import pygame
-
-# Initialize pygame mixer
-pygame.mixer.init()
-
-# Load your MP3 file
-pygame.mixer.music.load('output.mp3')
-
-# Play the MP3 file
-pygame.mixer.music.play()
-
-# Keep the script running until the music is playing
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
+print(pygame.__version__)
